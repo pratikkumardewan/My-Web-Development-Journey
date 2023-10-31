06334d162a6b3181bc19bc81e78cb87b63b5fbb3
coderpra <pratikkumardewan@gmail.com>
2023-11-01 04:38:27 +0530

    add all the previously created html, css, js and other file.

---
+++ 1 web/script.js
@@ -0,0 +1,19 @@
+alert("Welcome");
+console.log("Hello World");
+//primitive datatypes
+let a = 1;
+let b = 2.66;
+let c = BigInt("567");
+let d = null;
+let e = true;
+let f = 'c';
+let g = "hello";
+let h = Symbol("I am a coder");
+let i = undefined;
+console.log(a, b, c, d, e, f, g, h, i);
+console.log(typeof (i));
+console.log(typeof (c));
+c = 2;
+console.log(typeof (c));
+console.log(typeof (b));
+
