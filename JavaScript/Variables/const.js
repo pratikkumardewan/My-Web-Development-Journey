0dd7d836175f5402da46ba899ac670d0cbe6ff9f
coderpra <pratikkumardewan@gmail.com>
2024-05-21 13:21:09 +0530

    p

---
+++ JavaScript/Variables/const.js
@@ -0,0 +1,12 @@
+/*const: Variables declared with const cannot be reassigned.
+However, for objects and arrays declared with const, their properties or elements can be mutated.*/
+const a =45
+console.log(a)
+
+//this block will throw error
+// a="pratik"
+// console.log(a)
+// a= null
+// console.log(a)
+// a = undefined
+// console.log(a)
