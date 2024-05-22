56abb0a2ff640f4101fde5c5e25cdc5de4a460de
coderpra <pratikkumardewan@gmail.com>
2024-05-22 10:56:22 +0530

    p

---
+++ JavaScript/printingInConsole/HelloWorld.js
@@ -0,0 +1,11 @@
+/* multiple line
+comments */
+
+//single line comments
+
+// Using console.log()
+console.log("Hello", "World"); // Outputs: Hello World
+
+// Using process.stdout.write()
+process.stdout.write("Hello "); // No newline
+process.stdout.write("World\n"); // Newline
