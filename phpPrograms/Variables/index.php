f92447a1688cc9de9cb73077dffd39e10740da32
coderpra <pratikkumardewan@gmail.com>
2024-06-02 12:46:04 +0530

    p

---
+++ phpPrograms/Variables/index.php
@@ -0,0 +1,7 @@
+<?php
+    $name = "Pratik";
+    $age = 22;
+    $email = "fake@gmail.com";
+    echo "My name is {$name} and my age is {$age} .<br>";
+    echo "My email is {$email}."
+?>
--- phpPrograms/helloworld.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Hello, world!<br>";
-echo "My name is pratik";
-?>
